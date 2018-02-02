1e9a7ec021c61e5c01849041da4673a1ac68ad3b
dyerlab <rjdyer@vcu.edu>
2018-02-02 07:41:54 -0500

    making replacement

---
+++ Genetic Studio/Genetic Studio/ListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewController.swift
+//  Genetic Studio
+//
+//  Created by Rodney Dyer on 2/2/18.
+//  Copyright © 2018 Rodney Dyer. All rights reserved.
+//
+
+import Cocoa
+
+class ListViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
