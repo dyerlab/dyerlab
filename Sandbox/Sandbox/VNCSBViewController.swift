fc6710d95baecfba42a74b98423df3d823671c27
dyerlab <rjdyer@vcu.edu>
2018-02-07 11:21:13 -0500

    Added sandbox project to test UI components that will be shoved into the Frameworks as they mature.

---
+++ Sandbox/Sandbox/VNCSBViewController.swift
@@ -0,0 +1,24 @@
+//
+//  VNCSBViewController.swift
+//  Sandbox
+//
+//  Created by Rodney Dyer on 2/7/18.
+//  Copyright © 2018 Rodney Dyer. All rights reserved.
+//
+
+import Cocoa
+
+class VNCSBViewController: NSViewController {
+
+    @IBOutlet weak var vncTableView: NSTableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+        print("VNCSBViewController in EFFECT 'yal")
+    }
+    
+    
+}
