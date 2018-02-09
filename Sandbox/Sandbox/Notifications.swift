d174ad29374568c7d2762dd889e476efb952d42f
dyerlab <rjdyer@vcu.edu>
2018-02-09 08:52:41 -0500

    adding Notification.swift extension

---
+++ Sandbox/Sandbox/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  Sandbox
+//
+//  Created by Rodney Dyer on 2/8/18.
+//  Copyright © 2018 Rodney Dyer. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let loadVNCFileNotifiation = Notification.Name("loadVNCFile")
+}
