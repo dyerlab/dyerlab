7ec3e54d61e89d64c47a977a10b985cafd415737
dyerlab <rjdyer@vcu.edu>
2019-01-08 18:28:58 -0500

    updating connections for node outline widget

--- popgraphs/popgraphs/Sidebar/NodeOutlineWidget.swift
+++ popgraphs/popgraphs/Sidebar/NodeOutlineWidget.swift
@@ -10,14 +10,40 @@ import Cocoa
 
 class NodeOutlineWidget: NSView {
 
+    @IBOutlet var contentView: NodeOutlineWidget!
     @IBOutlet weak var nodeNameLabel: NSTextField!
     @IBOutlet weak var nodeSizeLabel: NSTextField!
     @IBOutlet weak var nodeDegreeLabel: NSTextField!
+    @IBOutlet weak var colorWell: NSColorWell!
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        commonInit()
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed("NodeOutlineWidget", owner: self, topLevelObjects: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [ .width, .height ]
+    }
+    
     
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
         // Drawing code here.
+    }
+    
+    public func setData( node: Node ) {
+        nodeNameLabel.stringValue = node.name!
+        nodeSizeLabel.stringValue = String("\(node.size.width)")
+        nodeDegreeLabel.stringValue = String("\(node.degree)")
     }
     
 }
